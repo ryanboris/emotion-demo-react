122ccb8e8c93c3f1369529bb876309b8908ac94c
ryanboris <ryan.boris@gmail.com>
2019-02-10 12:46:02 -0500

    💄 adds emotion

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,30 @@
+/** @jsx jsx */
 import React from 'react';
+import { css, jsx } from '@emotion/core';
+import styled from '@emotion/styled';
+
+const Wrap = styled.h1`
+  font-size: 5rem;
+  color: blue;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-family: 'Roboto', cursive;
+  font-weight: 100;
+  letter-spacing: 7px;
+`;
 
 export default function App() {
   return (
     <>
-      <h1>Sanity Test - its alive!</h1>
+      <Wrap
+        css={css`
+          padding: 1%;
+        `}
+      >
+        Sanity Test - its alive!
+      </Wrap>
     </>
   );
 }
